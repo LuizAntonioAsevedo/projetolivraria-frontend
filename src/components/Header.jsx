e32279e2f4ec4f9da93aff575f98b259a31a975d
Luiz Antônio Asevedo <laasevedo@gmail.com>
2026-05-31 11:22:01 -0300

    Corrige menu de navegação e rota de detalhes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,36 @@
-import { Link } from 'react-router-dom'
-import './Header.css'
-
-import logo from '../assets/logo.png'
+import { Link } from "react-router-dom";
+import "./Header.css";
+import logo from "../assets/logo.png";
 
 function Header() {
   return (
     <header className="header">
-      <div className="logo-area">
-        <img src={logo} alt="Logo" className="logo" />
+      <div className="logo-container">
+        <img
+          src={logo}
+          alt="Future and Knowledge Bookstore"
+          className="logo"
+        />
 
-        <h1>Future and Knowledge Bookstore</h1>
+        <div className="title-container">
+          <h1>Future and Knowledge Bookstore</h1>
+          <p className="slogan">
+            Explore o conhecimento através da leitura
+          </p>
+        </div>
       </div>
 
       <nav>
         <Link to="/">Home</Link>
 
-        <Link to="/details">Detalhes</Link>
-
         <Link to="/categoria">Categoria</Link>
+
+        <Link to="/details">Detalhes</Link>
 
         <Link to="/login">Login</Link>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
